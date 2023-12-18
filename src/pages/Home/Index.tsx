0c739e64000f5d842d08cce6356e932d4acd49c9
Pablo Henrique <pablohdsouza.dev@gmail.com>
2023-12-18 15:47:00 -0300

    feat: add isLoading state and try/catch block in fetchDecks function

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -5,9 +5,12 @@ import { Deck } from "../../@types/global";
 import { Plus } from "@phosphor-icons/react";
 import { NavLink } from "react-router-dom";
 import { Head } from "../../utils/Head";
+import { Loading } from "../../components/Loading/Index";
 
 export const Home = () => {
   const [decks, setDecks] = React.useState<Deck[] | null>(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
 
   async function deleteDeck(deckId: number) {
     if (!decks) return;
@@ -22,11 +25,28 @@ export const Home = () => {
 
   React.useEffect(() => {
     async function fetchDecks() {
-      const response = await api.get<Deck[]>("/deck");
-      setDecks(response.data);
+      try {
+        setIsLoading(true);
+        const response = await api.get<Deck[]>("/deck");
+        setDecks(response.data);
+      } catch (error) {
+        console.error(error);
+        setError(true);
+        setDecks(null);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchDecks();
   }, []);
+
+  if (isLoading) return <Loading />;
+  if (error)
+    return (
+      <h3 className="fade-right text-xl p-6">
+        Ops...Parece que ocorreu um erro! Por favor, atualize a página!
+      </h3>
+    );
 
   return (
     <div className="p-6 fade-right">
